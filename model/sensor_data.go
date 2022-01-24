700d8b794bfe41bf3abfccf3e043f3f72afd5a3e
Daniel Serleg <daniel.serleg@bitrise.io>
2022-01-24 21:14:25 +0100

    Remove command param

--- model/sensor_data.go
+++ model/sensor_data.go
@@ -2,7 +2,6 @@ package model
 
 type SensorData struct {
 	SensorID     string  `json:"sensor_id"`
-	Command      int     `json:"command"`
 	Temperature  float32 `json:"temperature"`
 	Humidity     float32 `json:"humidity"`
 	SoilMoisture float32 `json:"soil_moisture"`
